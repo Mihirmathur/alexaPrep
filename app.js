b172fc794f3e64b3fc4afd9b16bd8448eb13c44e
Mihirmathur <mihirmathur@g.ucla.edu>
2016-10-09 04:49:26 -0400

    Socket connection working

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ io.on('connection', function (socket) {
   socket.on('my other event', function (data) {
     console.log(data);
   });
+  socket.on('private message', function (from, msg) {
+    console.log('I received a private message by ', from, ' saying ', msg);
+  });
+
+  socket.on('disconnect', function () {
+    io.emit('user disconnected');
+  });
 });
 // var app = express();
 
